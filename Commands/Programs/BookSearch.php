e818d60965e5a23a73a2d758c7e6542f04d36586
AkinoJoey <gakiya213@gmail.com>
2023-10-31 16:46:11 +0900

    book-searchの完成

--- Commands/Programs/BookSearch.php
+++ Commands/Programs/BookSearch.php
@@ -5,11 +5,12 @@ namespace Commands\Programs;
 use Database\MySQLWrapper;
 use Commands\AbstractCommand;
 use Commands\Argument;
+use DateTime;
+use Exception;
 
 class BookSearch extends AbstractCommand{
     protected static ?string $alias = 'book-search';
     protected MySQLWrapper $mysqli;
-    protected static bool $requiredCommandValue = true;
 
     public function __construct()
     {
@@ -21,59 +22,93 @@ class BookSearch extends AbstractCommand{
 
     {
         return [
-        
+            (new Argument('isbn'))->description('引数のisbnを基に本の情報を検索します。')->required(true)->allowAsShort(true),
         ];
     }
 
     public function execute(): int
     {
-        $this->search();
+        $isbn = $this->getArgumentValue('isbn');
+        if($isbn === true){
+            throw new Exception("isbnの入力が必要です。");
+        }else{
+            $this->search($isbn);
+        }
+
         return 0;
     }
 
-    public function search() : void {
-        $isbn = $this->getCommandValue();
-        
+    public function search($isbn) : void {
         // テーブルを作成する
         $this->mysqli->query(file_get_contents(dirname(__FILE__,3) . '/Database/Examples/books.sql'));
 
-        // データがあるかどうか確認
-        $query = sprintf("SELECT data FROM books WHERE id = 'book-search-isbn-%s';", $isbn);
-        $data_exists = $this->mysqli->query($query);
-        print($query);
-        print_r($data_exists);
+        $id = 'book-search-isbn-' . $isbn;
 
-        if($data_exists === 0){
-            // データをAPIから取得して、挿入する
-            $url = sprintf('https://openlibrary.org/api/books?bibkeys=ISBN:%s&jscmd=data&format=json', $isbn);
-            $json_data = file_get_contents($url);
-            print("Book Data: " . PHP_EOL . $json_data . PHP_EOL);
+        // データベースにデータがあるかどうか確認
+        $book_data_assoc = $this->getBookData($id);
 
-            $key = sprintf('book-search-isbn-%s', $isbn);
-            $query = sprintf("INSERT INTO books (id, data) values ('%s', '%s')", $key, $json_data);
-            $this->mysqli->query($query);
-            
-        }elseif($data_exists === 1){
-            // created_atが30日以内か確認する
-            $query = sprintf("SELECT updated_at FROM books WHERE id = 'book-search-isbn-%s';", $isbn);
-            $updated_at = $this->mysqli->query($query);
-            echo $updated_at;
+        if(is_null($book_data_assoc)){
+            $this->saveBookDataToDatabase($isbn, $id);
+            $book_data_assoc = $this->getBookData($id);
+        }else{
+            $isWithin30days = $this->isWithin30days($book_data_assoc['updated_at']);
 
-            // データベースから取得して出力する
-            $query = sprintf("SELECT data FROM books WHERE id = 'book-search-isbn-%s';", $isbn);
-            $json_data = $this->mysqli->query($query);
-            // print("Book Data: " . PHP_EOL . $json_data . PHP_EOL);
-
+            if(!$isWithin30days){
+                $this->updateBookData($isbn, $id);
+                $book_data_assoc = $this->getBookData($id);
+            }
         }
+
+        $this->log($book_data_assoc['data']);
         
     }
+
+    public function saveBookDataToDatabase(string $isbn, string $id): void{
+        $json_data = $this->fetchApiBookData($isbn);
+
+        $query = "INSERT INTO books (id, data) VALUES (?, ?)";
+        $stmt = $this->mysqli->prepare($query);
+        $stmt->bind_param("ss", $id, $json_data);
+        $stmt->execute();
+        $stmt->close();
+    }
+
+    public function getBookData(string $id): ?array
+    {
+        $query = "SELECT data, updated_at FROM books WHERE id = ?";
+        $stmt = $this->mysqli->prepare($query);
+        $stmt->bind_param("s", $id);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+        $data = $result->fetch_assoc();
+        $stmt->close();
+        return $data;
+    }
+
+    public function updateBookData(string $isbn, string $key) : void {
+        $json_data = $this->fetchApiBookData($isbn);
+        $query = "UPDATE books SET data = ?, updated_at = CURRENT_TIMESTAMP WHERE id = ?";
+
+        $stmt = $this->mysqli->prepare($query);
+        $stmt->bind_param("ss", $json_data, $key);
+        $stmt->execute();
+        $stmt->close();
+    }
+
+    public function fetchApiBookData(string $isbn) : string {
+        $url = sprintf('https://openlibrary.org/api/books?bibkeys=ISBN:%s&jscmd=data&format=json', $isbn);
+        $json_data = file_get_contents($url);
+        return $json_data;
+    }
+
+    public function isWithin30days($date): bool{
+        $date = new DateTime($date);
+        $currentDate = new DateTime();
+        $diff = $date->diff($currentDate);
+        $daysDifference = $diff->days;
+        return $daysDifference <= 30;
+    }
+
+
 }
-
-// php console book-search --isbn 0385472579
-
-// ・キャッシュにデータがあるかどうか確認
-// ・ない場合はデータを取得して保存。
-// ・ある場合は、30日以上経過しているか確認
-// ・経過していない場合は、キャッシュのデータを使う
-// ・経過している場合は、データを取得して更新
-
