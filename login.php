0ef5871cb35e4d6ac1445bcbde09eb8ef699571d
AkinoJoey <gakiya213@gmail.com>
2023-11-09 12:24:31 +0900

    sqlインジェクションの確認

---
+++ login.php
@@ -0,0 +1,31 @@
+<?php
+spl_autoload_extensions(".php");
+spl_autoload_register();
+
+use Database\MySQLWrapper;
+
+$mysqli = new MySQLWrapper();
+$charset = $mysqli->get_charset();
+if ($charset === null) throw new Exception('Charset could be read');
+
+$username = $_POST['username'] ?? '';
+$password = $_POST['password'] ?? '';
+
+$query = "SELECT * FROM users WHERE username = '$username' AND password = '$password';";
+$result = $mysqli->query($query);
+$userData = $result->fetch_assoc();
+
+if ($userData) {
+    $login_username = $userData["username"];
+    $login_email = $userData["email"];
+    $login_role = $userData["role"];
+
+    echo "ログイン成功<br/>";
+    echo "こんにちは、$login_username<br/>";
+    if ($login_role == 'admin') {
+        echo "role: admin でログインしています。<br/>";
+        echo "password: $password<br/>";
+    }
+} else {
+    echo "ログイン失敗<br/>";
+}
